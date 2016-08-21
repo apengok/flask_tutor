d3dea22b26351c1bdc5df8067ed5e7712017c674
apengok <apengok@163.com>
2016-08-21 13:35:11 -0400

    follow the flasky

--- app/models.py
+++ app/models.py
@@ -1,43 +1,63 @@
-from hashlib import md5
-from app import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from flask_login import UserMixin
+from . import db,login_manager
 
-class User(db.Model):
+
+class Role(db.Model):
+    __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key=True)
-    nickname = db.Column(db.String(64),index=True,unique=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>'%self.name
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+
     email = db.Column(db.String(120),index=True,unique=True)
-    posts = db.relationship('Post',backref='author',lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean,default=False)
+    #nickname = db.Column(db.String(64),index=True,unique=True)
+    #posts = db.relationship('Post',backref='author',lazy='dynamic')
+    #about_me = db.Column(db.String(140))
+    #last_seen = db.Column(db.DateTime)
 
     @property
-    def is_authenticated(self):
-        return True
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
-    @property
-    def is_active(self):
-        return True
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
 
-    @property
-    def is_annonymous(self):
-        return False
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
-    def get_id(self):
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+
+    def confirm(self,token):
+        s = Serializer('hard to guess string' )
         try:
-            return unicode(self.id)
-        except NameError:
-            return str(self.id) #python 3
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
-        return '<User %r>'%(self.nickname)
+        return '<User %r>' % self.username
 
-    def avatar(self,size):
-        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d'%(md5(self.email.encode('utf-8')).hexdigest(),size)
-
-class Post(db.Model):
-    id = db.Column(db.Integer,primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- config.py
+++ config.py
@@ -1,15 +1,52 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repository')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repository')
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+#WTF_CSRF_ENABLED = True
+#SECRET_KEY = 'you-will-never-guess'
 
-OPENID_PROVIDERS = [
-        {'name':'Google','url':'https://www.google.com/accounts/o8/id'},
-        {'name':'Yahoo','url':'https://me.yahoo.com'},
-        {'name':'AOL','url':'http://openid.aol.com/<username>'},
-        {'name':'Flickr','url':'http://flickr.com/<username>'},
-        {'name':'MyOpenID','url':'https://www.myopenid.com'}]
+#OPENID_PROVIDERS = [
+#        {'name':'Google','url':'https://www.google.com/accounts/o8/id'},
+#        {'name':'Yahoo','url':'https://me.yahoo.com'},
+#        {'name':'AOL','url':'http://openid.aol.com/<username>'},
+#        {'name':'Flickr','url':'http://flickr.com/<username>'},
+#        {'name':'MyOpenID','url':'https://www.myopenid.com'}]
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <apengok@163.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')  or \
+            'sqlite:///' + os.path.join(basedir,'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \
+            'sqlite:///' + os.path.join(basedir,'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir,'data.sqlite')
+
+config = {
+        'development':DevelopmentConfig,
+        'testing':TestingConfig,
+        'production':ProductionConfig,
+        'default':DevelopmentConfig
+        }
